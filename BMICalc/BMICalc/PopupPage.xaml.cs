c6a100edb4e7223bb9f0a8f0f7a154b3a9ca8da2
Łukasz Birski <01029674@pw.edu.pl>
2020-07-26 18:45:08 +0200

    Popup to choose units system was improved and now returns chosen system

--- BMICalc/BMICalc/PopupPage.xaml.cs
+++ BMICalc/BMICalc/PopupPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Toast;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,5 +18,29 @@ namespace BMICalc
             InitializeComponent();
         }
 
+        private void metricUnitsCheckbox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            usUnitsCheckbox.IsChecked = !metricUnitsCheckbox.IsChecked;
+        }
+
+        private void usUnitsCheckbox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            metricUnitsCheckbox.IsChecked = !usUnitsCheckbox.IsChecked;
+        }
+
+        private void saveButton_Clicked(object sender, EventArgs e)
+        {
+            if(metricUnitsCheckbox.IsChecked || usUnitsCheckbox.IsChecked)
+            {
+                string alert = returnCheckedCheckBox();
+                CrossToastPopUp.Current.ShowToastError(alert, Plugin.Toast.Abstractions.ToastLength.Short);
+            }
+        }
+
+        private string returnCheckedCheckBox()
+        {
+            if (usUnitsCheckbox.IsChecked) return "US_UNITS";
+            else return "METRIC_UNITS";
+        }
     }
 }
