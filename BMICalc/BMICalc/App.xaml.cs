c90a75d13345f823cfdb2eb22730ce488c82598c
Łukasz Birski <01029674@pw.edu.pl>
2020-08-07 11:38:24 +0200

    AppCenter libraries and App.Center.Start() were added to the project

--- BMICalc/BMICalc/App.xaml.cs
+++ BMICalc/BMICalc/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace BMICalc
 {
@@ -15,6 +18,10 @@ namespace BMICalc
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=876be569-3a46-4a6f-9f33-95e0eb1ac6ba;" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios={Your iOS App secret here}",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
